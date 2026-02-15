3df6b7bcd096e701d843ca9686f3612abf0526a3
anymora <anymora.de@gmail.com>
2026-02-15 22:06:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -915,8 +915,8 @@ app.get("/tee-black-preview", async (req, res) => {
       artworkUrl,
       baseMockupUrl,
       mockupUrl: TEE_BLACK_MOCKUP_URL,
-      scale: 0.36,
-      offsetX: 0.32,
+      scale: 0.38,
+      offsetX: 0.31,
       offsetY: 0.27,
       overlayUrl: TEE_BLACK_OVERLAY_URL,
     });
